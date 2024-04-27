fe2ce268f0d35802a0df34b9de8a7bb9bfa8ba5f
Gautam Bisht <bishtgautam012@gmail.com>
2024-04-28 00:54:34 +0530

    Navbar fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -39,16 +39,13 @@ const Navbar = ({ className }: { className?: string }) => {
             ></MenuItem>
           </Link>
 
-          <MenuItem setActive={setActive} active={active} item="About">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink target="_blank" href="http://gautambisht.rf.gd/">
-                About Me{" "}
-              </HoveredLink>
-              <HoveredLink target="_blank" href="/about-thisproject">
-                About This Project{" "}
-              </HoveredLink>
-            </div>
-          </MenuItem>
+          <Link target="_blank" href={"http://gautambisht.rf.gd/"}>
+            <MenuItem
+              setActive={setActive}
+              active={active}
+              item="About Me"
+            ></MenuItem>
+          </Link>
         </Menu>
       </div>
     </>
